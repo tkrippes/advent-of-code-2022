7c21c0d181ec98aa89b86d2c5a0c7d5d0f140a65
Tom Krippes <tkrippes@live.de>
2022-12-04 14:02:21 +0100

    Create day_3.py

---
+++ day_3/day_3.py
@@ -0,0 +1,33 @@
+def get_shared_character(string_1, string_2):
+    # search for common character
+    for character in string_1:
+        if character in string_2:
+            return character
+    
+    # if no common character, return empty string
+    return ''
+
+def priority_of_character(character):
+    # lowercase letter
+    if ord(character) >= ord('a') && ord(character) <= ord('z'):
+        return ord(character) - ord('a') + 1
+    # uppercase letter
+    elif ord(character) >= ord('A') && ord(character) <= ord('Z'):
+        return ord(character) - ord('A') + 27
+    else:
+        return 0
+
+with open('data_3_input.txt') as rucksacks:
+    # init variables
+    lines = rucksacks.readlines()
+    sum_of_priorities = 0
+    
+    # calculate sum of priorities
+    for line in lines:
+        first_compartment_items = line[:len(line / 2)]
+        second_compartment_items = line[len(line / 2):]
+        character = get_shared_character(first_compartement_items, second_compartment_item)
+        sum_of_priorities += priority_of_character(character)
+        
+    # result 1
+    print(sum_of_priorites)
